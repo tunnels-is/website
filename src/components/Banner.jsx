ae71ce7a6563dddf7f8af862b0467f04e3dfa026
M Alvee <M.Alvee8141@gmail.com>
2025-06-01 21:58:25 +0600

    ui: updates to the styles

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React from "react";
+import styles from './Banner.module.css';
 
 import tunnelsEditor from "../assets/screens/tunnels-editor.png";
 import orgsFinal from "../assets/screens/orgs-final.png";
@@ -20,6 +21,12 @@ const galleryImages = [
     width: 800,
     height: 450,
     caption: "Custom DNS",
+  },
+	{
+    src: darkMode,
+    width: 800,
+    height: 450,
+    caption: "Dark Mode",
   },
    {
     src: darkMode,
@@ -51,39 +58,53 @@ const galleryImages = [
     height: 450,
     caption: "DNS Block",
   },
-  {
-    src: darkMode,
-    width: 800,
-    height: 450,
-    caption: "Dark Mode",
-  },
+  
 ];
 
 const Banner = (props) => {
-
-	useEffect(() => {
-			$('#myMosaic').Mosaic({ 
-				innerGap: 10
-	});
-	}, [])
-
-	return (<div className={"banner viewport-hifde " + props.tag1}>
-
-		<div className="top-text">
-			<h1 className="text bigtitle">{props.title}</h1>
-			<p className="subtitle" dangerouslySetInnerHTML={{ __html: props.subtitle }}>
-			</p>
-		</div>
-
-	
-		<div id="myMosaic">
-        {galleryImages.map((image, index) => (
-						<img src={image.src} width={image.width} height={image.height}/>
-				))}
+  return (
+    <div className={`${styles.banner} ${props.tag1}`}>
+      <div className={styles.topText}>
+        <h1 className="text bigtitle text-black">{props.title}</h1>
+        <p className="subtitle" dangerouslySetInnerHTML={{ __html: props.subtitle }}></p>
       </div>
 
-	</div>)
-}
+      <div className={styles.imageGrid}>
+        {galleryImages.map((image, index) => {
+          let containerClassName = styles.imageContainer;
+          
+          // Assign different grid areas based on index
+          if (index === 0) {
+            containerClassName += ` ${styles.tall1}`; // First image takes 3 rows
+          } else if (index === 1) {
+            containerClassName += ` ${styles.normal}`; // Single cell
+          } else if (index === 2) {
+            containerClassName += ` ${styles.wide1}`; // Wide image spanning 2 columns
+          } else if (index === 3) {
+            containerClassName += ` ${styles.tall2}`; // Second tall image spanning 2 rows
+          } else if (index === 4) {
+            containerClassName += ` ${styles.normal}`; // Single cell
+          } else if (index === 5) {
+            containerClassName += ` ${styles.wide2}`; // Second wide image spanning 2 columns
+          } else {
+            containerClassName += ` ${styles.normal}`; // All others are normal single cells
+          }
+
+          return (
+            <div key={index} className={containerClassName}>
+              <img 
+                src={image.src} 
+                alt={image.caption}
+                className={styles.image}
+              />
+              <div className={styles.caption}>{image.caption}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Banner;
 
