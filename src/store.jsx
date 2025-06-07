3386fd656e7b84db87e66f2669b977231f0324c5
zveinn <info@sveinn.is>
2025-06-07 11:26:25 -0500

    2.0.0

--- src/store.jsx
+++ src/store.jsx
@@ -1,7 +1,7 @@
 const DATA = "data_";
 import { LayersIcon } from '@radix-ui/react-icons';
 import axios from 'axios';
-const Version = "1.0.15"
+const Version = "2.0.0"
 
 var STORE = {
 	Name: "Tunnels",
@@ -89,8 +89,8 @@ var STORE = {
 			Title: "Download",
 			Subtitle: "click here for all available releases",
 			AllLink: "https://github.com/tunnels-is/tunnels/releases/latest",
-			MacOS: "https://github.com/tunnels-is/tunnels/releases/download/v" + Version + "/tunnels_" + Version + "_Darwin_arm64.tar.gz",
-			Windows: "https://github.com/tunnels-is/tunnels/releases/download/v" + Version + "/tunnels_" + Version + "_Windows_x86_64.zip",
+			MacOS: "https://github.com/tunnels-is/tunnels/releases/download/" + Version + "/tunnels_" + Version + "_Darwin_arm64.tar.gz",
+			Windows: "https://github.com/tunnels-is/tunnels/releases/download/" + Version + "/tunnels_" + Version + "_Windows_x86_64.zip",
 			Linux: "https://github.com/tunnels-is/tunnels/releases/latest",
 		},
 		Pricing: {
